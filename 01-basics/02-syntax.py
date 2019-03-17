0ae292250e8d2e7f31b3db006612ad79709b0465
jdxlabs <jdevoucoux@gmail.com>
2019-03-17 13:09:07 +0100

    linter

--- 01-basics/02-syntax.py
+++ 01-basics/02-syntax.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 # That's how you should write a comment
-
 """ Or in this form
     if you need multiple lines
 """
 
 # if you execute `python3 myfile.py`,you will see "Hello world !"
 print('Hello world !')
+
+# ==> talk about linter..
+# https://pylint.org/
+# https://www.python.org/dev/peps/pep-0008/
+# https://code.visualstudio.com/docs/python/linting
+# https://pep8.readthedocs.io/en/latest/intro.html#configuration
 
