30efe71b8720b9b4652182dd20c0907e3a6e2900
jdxlabs <jdevoucoux@gmail.com>
2019-03-23 23:41:34 +0100

    update

--- 01-basics/04-numbers.py
+++ 01-basics/04-numbers.py
@@ -3,10 +3,25 @@
 nb = 42
 print(nb)
 
-type(nb)
-# <class 'int'>
+print(type(nb))
+#   <class 'int'>
 
-# operators
+print(type(42.0))
+#   <class 'float'>
+
+print(type(42j))
+#   <class 'float'>
+
+
+# operators and some math basics
+
+x = 1
+y = 2
+z = 3
+i = 2.4
+j = 0.1
+k = -4
+
 # 1 + 1   # => 2
 # 8 - 1   # => 7
 # 10 * 2  # => 20
