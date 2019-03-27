cab47ac0a77f255f962f88ae039e460c254085e6
jdxlabs <jdevoucoux@gmail.com>
2019-03-27 08:06:45 +0100

    readme + for loops

--- 01-basics/06-loops.py
+++ 01-basics/06-loops.py
@@ -1,1 +1,56 @@
 #!/usr/bin/env python3
+
+# you can use the while loop
+# be carrefull to not create infinite loops with it
+x = 1
+while x <= 3:
+    print(x)
+    x += 1
+
+# # you can use the break statement
+# to conditionnaly exit the loop
+x = 1
+while x <= 3:
+    print(x)
+    if x == 1:
+        break
+    x += 1
+
+# or the continue statement
+# to conditionnaly go to the next iteration of the loop
+x = 1
+while x <= 20:
+    x += 1
+    if x % 2 == 0: # is pair
+        continue
+    print(x)
+
+# you can use the for loop
+items = ["pears", "apples", "bananas"]
+for item in items:
+    print(item)
+
+# break and continue statements
+# can be used in the for loop too
+
+# The range() function returns a sequence of numbers, starting from 0
+for x in range(3):
+    print(x)
+
+# however it is possible to specify the starting value by adding a parameter
+for x in range(10, 15):
+    print(x)
+
+# range allows you to define to define the incrementation
+# with the third parameter, here is an increment of 5
+for x in range(10, 25, 5):
+    print(x)
+
+# you can use the else statement with the for loop
+# complementary to the break statement
+for x in range(3):
+    if x == 30:
+        break
+    print(x)
+else:
+    print("not catched by the break statement")
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# learn-python3
-Learn the Python 3.x programming langage
+# Learn the Python 3.x programming langage
+
+> Like a kung fu master, a Pythonista knows how to kill with a single finger, and never to actually do it.
+
+Python is a very consise and efficient language,  
+it allows you to work in different domains :  
+simple scripting, web development, datasciences, gui interfaces, etc.  
+and to write portable code for every platforms (linux, mac, windows)  
+
+It's a good starting point to learn programming in general  
+because it is a multi-paradigm language  
+and will let you explore a lot of different patterns.  
+
+Have fun with this language,  
+May this lessons help you in the path to masterize Python.  
